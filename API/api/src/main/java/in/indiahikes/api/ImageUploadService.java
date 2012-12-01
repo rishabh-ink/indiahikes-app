cb1013b3292b51d5d786aa2eacea0a393246b141
Srinivas Iyengar <srini156@gmail.com>
2012-12-01 20:10:18 +0530

    Changes to ImageUploadService

--- API/api/src/main/java/in/indiahikes/api/ImageUploadService.java
+++ API/api/src/main/java/in/indiahikes/api/ImageUploadService.java
@@ -1,14 +1,12 @@
 package in.indiahikes.api;
 
 import in.indiahikes.api.data.ApiResponse;
+import in.indiahikes.flickr.ImageHandler;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -29,44 +27,31 @@ public class ImageUploadService
 	@POST
 	@Consumes (MediaType.MULTIPART_FORM_DATA)
 	@Produces (MediaType.APPLICATION_JSON)
-	public Response addNewClientImage(@HeaderParam ("Content-Length") Long fileSize,
+	public Response uploadImage(@HeaderParam ("Content-Length") Long fileSize,
 	        @FormDataParam ("file") InputStream inputStream,
 	        @FormDataParam ("file") FormDataContentDisposition fileDetails,
-	        @FormDataParam ("file") FormDataBodyPart bodyPart)
-	{
-		String uploadedFileLocation = "./" + fileDetails.getFileName();
-
-		// save it
-		writeToFile(inputStream, uploadedFileLocation);
-
-		String output = "File uploaded to : " + uploadedFileLocation;
-		return Response.ok().entity(new ApiResponse(Response.Status.OK, output)).build();
-	}
-
-	// save uploaded file to new location
-	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation)
+	        @FormDataParam ("file") FormDataBodyPart bodyPart,
+	        @FormDataParam ("latitude") @DefaultValue ("0") String latitude,
+	        @FormDataParam ("longitude") @DefaultValue ("0") String longitude,
+	        @FormDataParam ("accuracy") @DefaultValue ("0") String accuracy)
 	{
 
+		ImageHandler imageHandler = new ImageHandler();
+		String output = "";
 		try
 		{
-			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			out = new FileOutputStream(new File(uploadedFileLocation));
-			while ((read = uploadedInputStream.read(bytes)) != -1)
-			{
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
+			output =
+			        imageHandler.uploadImageToFlickr(inputStream, fileDetails.getFileName(), latitude, longitude,
+			                accuracy);
 		}
-		catch (IOException e)
+		catch (Exception ex)
 		{
-
-			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+			        .entity(new ApiResponse(Response.Status.INTERNAL_SERVER_ERROR, "Upload to Flickr Failed")).build();
 		}
+		// save it
+		// writeToFile(inputStream, uploadedFileLocation);
 
+		return Response.ok().entity(new ApiResponse(Response.Status.OK, output)).build();
 	}
-
 }
